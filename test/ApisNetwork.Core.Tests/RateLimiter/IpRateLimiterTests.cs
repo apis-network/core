fca1fcc31ea39f662557cb60ade241ea5c13027e
Joan Caron <caronjoan@gmail.com>
2018-11-11 19:30:02 +0100

    ip rate limiter tests

---
+++ test/ApisNetwork.Core.Tests/RateLimiter/IpRateLimiterTests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using ApisNetwork.Core.RateLimiter;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+
+using Moq;
+
+using Xunit;
+
+namespace ApisNetwork.Core.Tests.RateLimiter
+{
+    public class IpRateLimiterTests
+    {
+        private readonly TimeSpan _defaultCacheDuration = TimeSpan.FromMilliseconds(10);
+
+        private readonly IpRateLimiter _ipRateLimiter;
+
+        private readonly Mock<HttpRequest> _requestMock;
+
+        public IpRateLimiterTests()
+        {
+            var ip = new StringValues("1.1.1.1");
+            this._requestMock = new Mock<HttpRequest>();
+            this._requestMock.Setup(request => request.Headers.TryGetValue(It.IsAny<string>(), out ip)).Returns(true);
+            this._ipRateLimiter = new IpRateLimiter(
+                new MemoryCache(new MemoryCacheOptions()),
+                this._defaultCacheDuration);
+        }
+
+        [Fact]
+        public async Task ShouldBlockRegisteredIps()
+        {
+            await this._ipRateLimiter.CanExecuteAsync(this._requestMock.Object);
+            var canExecute = await this._ipRateLimiter.CanExecuteAsync(this._requestMock.Object);
+            Assert.False(canExecute);
+        }
+
+        [Fact]
+        public async Task ShouldNotBlockNewIps()
+        {
+            var canExecute = await this._ipRateLimiter.CanExecuteAsync(this._requestMock.Object);
+            Assert.True(canExecute);
+        }
+
+        [Fact]
+        public async Task ShouldNotBlockRegisteredIpsAfterCacheExpiration()
+        {
+            await this._ipRateLimiter.CanExecuteAsync(this._requestMock.Object);
+            Thread.Sleep(this._defaultCacheDuration);
+            var canExecute = await this._ipRateLimiter.CanExecuteAsync(this._requestMock.Object);
+            Assert.True(canExecute);
+        }
+    }
+}
